f1383232fa3184238576989296a9490dab6263f5
Gokul <gokul.k.03.07@gmail.com>
2026-08-18 23:13:09 +0530

    Fix task model import case

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Task = require("../models/Task");
+const Task = require("../models/task");
 const User = require("../models/user");
 const authMiddleware = require("../middleware/authMiddleware");
 
