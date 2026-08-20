f8c1fe8b637eca196e74d1964a69af6ccbd38f03
Gokul <gokul.k.03.07@gmail.com>
2026-08-20 21:33:21 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Render backend URL
 const api = axios.create({
-    baseURL: "https://focusmate-0fbg.onrender.com/api",
+    baseURL: "https://focusmate-backend-kgnu.onrender.com",
     headers: {
         "Content-Type": "application/json"
     }
